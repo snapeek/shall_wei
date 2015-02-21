ae4a2f7732f5f45d4e679b59a2ac333959c5569c
karma <boris.isolet@gmail.com>
2015-02-21 21:42:51 +0800

    added

--- lib/nlpir/ictclas.rb
+++ lib/nlpir/ictclas.rb
@@ -149,9 +149,10 @@ module Nlpir
 
     def NLPIR_Init(sInitDirPath=nil , encoding=UTF8_CODE)
       sInitDirPath += "/Data/"
+      binding.pry
+      filemother = File.expand_path("../Data", __FILE__)
       if File.exist?(sInitDirPath)==false
         FileUtils.mkdir(sInitDirPath)
-        filemother = File.expand_path("../Data", __FILE__)
         p "copy file #{filemother} => #{sInitDirPath}"
         FileUtils.copy_entry filemother, sInitDirPath
       end
