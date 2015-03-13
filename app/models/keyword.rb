507193e49366dc56ef233826dfee2c245c99e1c2
karma <boris.isolet@gmail.com>
2015-03-13 16:48:00 +0800

    added

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -31,10 +31,20 @@ class Keyword
 
   def w_to_csv
     CSV.open("tmp/csv/#{content}-微博-#{Time.at(starttime).strftime('%F')}-#{Time.at(endtime).strftime('%F')}.csv", "wb") do |csv|
-      csv << ["用户名", "内容", "发表时间", "月", "天", "小时","转载数", "个人认证", "商业认证", ""]
+      csv << ["用户名", "内容", "发表时间", "月", "天", "小时","转载数", "个人认证", "商业认证", "感情色彩"]
       weibos.each do |w|
         tt = Time.at(w.created_at)
         csv << [w.user_name, w.content, tt, tt.month, tt.day, tt.hour, w.reposts_count, w.approve, w.approve_co]
+      end
+    end
+  end
+
+  def w2_to_csv
+    CSV.open("tmp/csv/#{content}-微博-#{Time.at(starttime).strftime('%F')}-#{Time.at(endtime).strftime('%F')}.csv", "wb") do |csv|
+      csv << ["用户名", "内容", "发表时间", "转载数", "评论数"]
+      weibos.each do |w|
+        tt = Time.at(w.created_at)
+        csv << [w.user_name, w.content, tt.strftime("%F %T") , w.reposts_count, w.comments_count]
       end
     end
   end
