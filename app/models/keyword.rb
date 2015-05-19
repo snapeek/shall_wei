480c837e8848ebdb764e030c6c414e90d582b976
karma <boris.isolet@gmail.com>
2015-05-19 14:06:07 +0800

    added w2csv gbk

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -33,7 +33,9 @@ class Keyword
 
   def w_to_csv
     CSV.open("tmp/csv/#{content}-微博-#{Time.at(starttime).strftime('%F')}-#{Time.at(endtime).strftime('%F')}.csv", "wb") do |csv|
-      csv << ["用户名", "内容", "发表时间", "月", "天", "小时","转载数", "个人认证", "商业认证", "感情色彩"].map{|str| str.encode('utf-8', 'gbk')}
+      csv << ["用户名", "内容", "发表时间", "月", "天", "小时","转载数", "个人认证", "商业认证", "感情色彩"].map do |str| 
+        str.encode('utf-8', 'gbk',{:invalid => :replace, :undef => :replace, :replace => '?'}
+      end
       weibos.each do |w|
         tt = Time.at(w.created_at)
         csv << [w.user_name, w.content, tt, tt.month, tt.day, tt.hour, w.reposts_count, b2i(w.approve), b2i(w.approve_co)].map do |str|
