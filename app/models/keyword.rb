f10d272306b06b1f87484745ef245bb115dae4e2
karma <boris.isolet@gmail.com>
2015-05-25 08:59:06 +0800

    added

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -30,7 +30,7 @@ class Keyword
     weibo_to_csv
     repost_to_csv
   end
-    WWW = ["强迫消费","不明码标价","假冒伪劣","混乱","态度差","强买强卖","欺客宰客","卫生差","不文明","黑导游","旅游厕所","物价高","尾随兜售","一日游","诱导购物"]
+  WWW = ["强迫消费","不明码标价","假冒伪劣","混乱","态度差","强买强卖","欺客宰客","卫生差","不文明","黑导游","旅游厕所","物价高","尾随兜售","一日游","诱导购物"]
   def w_to_csv
     CSV.open("tmp/csv/#{content}-微博-#{Time.at(starttime).strftime('%F')}-#{Time.at(endtime).strftime('%F')}.csv", "wb") do |csv|
       csv << ["用户名", "内容", "发表时间", "月", "天", "小时","转载数", "个人认证", "商业认证", "感情色彩"].map do |str| 
@@ -51,7 +51,7 @@ class Keyword
           str.to_s.encode('gbk','utf-8',{:invalid => :replace, :undef => :replace, :replace => '?'})
         end
       end      
-    en
+    end
   end
 
   def b2i(b)
