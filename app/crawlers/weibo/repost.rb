243a7dddca05ace5991eba40120199096b84b86c
karma <boris.isolet@gmail.com>
2015-07-23 23:53:47 +0800

    added

--- app/crawlers/weibo/repost.rb
+++ app/crawlers/weibo/repost.rb
@@ -12,8 +12,8 @@ module WeiboUtils
         w[:wid] = w[:mid] = str_to_mid(url.split('/').last)
         w[:text] = get_field(page, '.WB_detail .WB_text').text
         w[:created_at] = get_field(page, 'div.WB_from.S_txt2>a'){|a| Time.parse(a.attr('title')).to_i }
-        weibo = w[:mid] ? Weibo.find_or_create_by(:mid => w[:mid]) : Weibo.new
-        weibo = weibo.update(w)
+        # weibo = w[:mid] ? Weibo.find_or_create_by(:mid => w[:mid]) : Weibo.new
+        weibo = weibo.create(w)
         weibo.save
         repost(weibo.mid)
       end
